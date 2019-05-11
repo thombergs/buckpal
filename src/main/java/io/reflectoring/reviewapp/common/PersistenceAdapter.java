926453cb70753d0f5bafdc9c03a7cf9a910fb32a
Tom Hombergs <tom.hombergs@gmail.com>
2019-05-11 06:48:13 +0200

    added stereotype annotations

---
+++ src/main/java/io/reflectoring/reviewapp/common/PersistenceAdapter.java
@@ -0,0 +1,27 @@
+package io.reflectoring.reviewapp.common;
+
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Component
+public @interface PersistenceAdapter {
+
+  /**
+   * The value may indicate a suggestion for a logical component name,
+   * to be turned into a Spring bean in case of an autodetected component.
+   * @return the suggested component name, if any (or empty String otherwise)
+   */
+  @AliasFor(annotation = Component.class)
+  String value() default "";
+
+}
